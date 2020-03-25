78de06fd16c64c0d778b16376caa6b618295a6ed
Katy DeCorah <decorah@mapbox.com>
2020-03-25 09:58:55 -0400

    move Heading into own file

---
+++ docs/components/api/heading.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IconText from '@mapbox/mr-ui/icon-text';
+
+export default class Heading extends React.Component {
+    render() {
+        const { section } = this.props;
+        return (
+            <div className="mb24">
+                <h3 className="mb12" id={section.namespace.toLowerCase()}>
+                    <a
+                        className="unprose color-blue-on-hover"
+                        href={`#${section.namespace.toLowerCase()}`}
+                    >
+                        {section.name}
+                    </a>
+                </h3>
+                {section.context && section.context.github && (
+                    <a
+                        className="pt6 link--gray txt-s unprose"
+                        href={section.context.github.url}
+                    >
+                        <IconText iconBefore="github">
+                            {section.context.github.path}
+                        </IconText>
+                    </a>
+                )}
+            </div>
+        );
+    }
+}
+
+Heading.propTypes = {
+    section: PropTypes.shape({
+        namespace: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        context: PropTypes.shape({
+            github: PropTypes.shape({
+                url: PropTypes.string.isRequired,
+                path: PropTypes.string.isRequired
+            })
+        })
+    }).isRequired
+};
--- docs/components/api/item.js
+++ docs/components/api/item.js
@@ -6,7 +6,7 @@ import GithubSlugger from 'github-slugger';
 import { highlightJavascript } from '../../components/prism_highlight.js';
 import docs from '../api.json'; // eslint-disable-line
 import ApiItemMember from './item-member';
-import IconText from '@mapbox/mr-ui/icon-text';
+import Heading from './heading';
 import Parameters from './parameters';
 import Properties from './properties';
 import Returns from './returns';
@@ -43,47 +43,6 @@ Augments.propTypes = {
     section: PropTypes.shape({
         augments: PropTypes.array
     })
-};
-
-class Heading extends React.Component {
-    render() {
-        const { section } = this.props;
-        return (
-            <div className="mb24">
-                <h3 className="mb12" id={section.namespace.toLowerCase()}>
-                    <a
-                        className="unprose color-blue-on-hover"
-                        href={`#${section.namespace.toLowerCase()}`}
-                    >
-                        {section.name}
-                    </a>
-                </h3>
-                {section.context && section.context.github && (
-                    <a
-                        className="pt6 link--gray txt-s unprose"
-                        href={section.context.github.url}
-                    >
-                        <IconText iconBefore="github">
-                            {section.context.github.path}
-                        </IconText>
-                    </a>
-                )}
-            </div>
-        );
-    }
-}
-
-Heading.propTypes = {
-    section: PropTypes.shape({
-        namespace: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        context: PropTypes.shape({
-            github: PropTypes.shape({
-                url: PropTypes.string.isRequired,
-                path: PropTypes.string.isRequired
-            })
-        })
-    }).isRequired
 };
 
 class ApiItem extends React.Component {
