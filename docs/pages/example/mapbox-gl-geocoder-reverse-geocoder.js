d177263bdfab47ea887150ac10f2f1884ea632b5
ZiZi <zmiles@gwu.edu>
2020-04-22 12:14:44 -0400

    created new reverse-geocoder example

---
+++ docs/pages/example/mapbox-gl-geocoder-reverse-geocoder.js
@@ -0,0 +1,11 @@
+/*---
+title: Search using Coordinates or an Address
+description: Use the geocoder to search for addresses or coordinates buildings within your map.
+tags:
+  - geocoder
+pathname: /mapbox-gl-js/example/mapbox-gl-geocoder-reverse-geocoder/
+---*/
+import Example from '../../components/example';
+import html from './mapbox-gl-geocoder-reverse-geocoder.html';
+
+export default Example(html);
